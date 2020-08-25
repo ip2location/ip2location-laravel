a2c8d6509d05930b2ab05e32e5621066a5185b89
IP2Location <support@ip2location.com>
2020-08-25 16:36:14 +0800

    Added IP2Location web service.

--- src/IP2LocationLaravel.php
+++ src/IP2LocationLaravel.php
@@ -4,19 +4,31 @@ namespace Ip2location\IP2LocationLaravel;
 
 class IP2LocationLaravel
 {
-
-    public function get($ip)
+    public function __construct()
     {
-        $db = new \IP2Location\Database($this->getDatabasePath(), \IP2Location\Database::FILE_IO);
-
-        $records = $db->lookup($ip, \IP2Location\Database::ALL);
-
-        return $records;
+        $this->db = new \IP2Location\Database($this->getDatabasePath(), \IP2Location\Database::FILE_IO);
     }
 
     private function getDatabasePath()
     {
         return config('ip2locationlaravel.ip2location.local.path');
+    }
+
+    public function get($ip)
+    {
+        $records = $this->db->lookup($ip, \IP2Location\Database::ALL);
+        
+        return $records;
+    }
+    
+    public function query($ip, $apikey, $package = 'WS1', $ssl = false, $addons = [], $language = 'en') 
+    {
+        
+        $ws = new \IP2Location\WebService($apikey, $package, $ssl);
+        
+        $records = $ws->lookup($ip, $addons, $language);
+        
+        return $records;
     }
 
 }
